cc9ea917a8bdbb91f4760c6c8625f0fe387201da
Milena Rogala <milena.rogala.qa@protonmail.com>
2020-05-03 19:59:29 +0200

    Zadanie: deklarowanie i wywołanie zmiennych - wersja poprawiona

--- kodilla-intro/src/main/java/MainExtended.java
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -1,10 +1,17 @@
 public class MainExtended {
-    String productName  = "Bicycle";
-    double price = 399.99;
-    boolean hasBasket = false;
-    String colour = "red";
-    char quality = 'P';
-    int stock = 123;
+    public static void main(String[] args){
+        String productName  = "Bicycle";
+        double price = 399.99;
+        boolean hasBasket = false;
+        String colour = "red";
+        char quality = 'P';
+        int stock = 123;
 
-
+        System.out.println(productName);
+        System.out.println(price);
+        System.out.println(hasBasket);
+        System.out.println(colour);
+        System.out.println(quality);
+        System.out.println(stock);
+    }
 }
