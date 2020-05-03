f15bc2ff8429eb9b7560554af980478005266e2a
Milena Rogala <milena.rogala.qa@protonmail.com>
2020-05-03 14:35:04 +0200

    Zadanie: Hello World!

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+        System.out.println("Milena");
+        System.out.println(35);
+        System.out.println('R');
+
+    }
+}
