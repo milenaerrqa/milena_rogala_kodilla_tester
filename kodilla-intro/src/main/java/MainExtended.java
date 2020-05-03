76de4683eadf346c713be3468c95bf03317b837c
Milena Rogala <milena.rogala.qa@protonmail.com>
2020-05-03 14:51:52 +0200

    Zadanie: deklarowanie i wywołanie zmiennych

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,10 @@
+public class MainExtended {
+    String productName  = "Bicycle";
+    double price = 399.99;
+    boolean hasBasket = false;
+    String colour = "red";
+    char quality = 'P';
+    int stock = 123;
+
+
+}
