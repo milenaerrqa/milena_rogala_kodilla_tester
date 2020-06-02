0d83c7142d33f97e292dc77975deaa9441075507
Milena Rogala <milena.rogala.qa@protonmail.com>
2020-06-02 21:28:09 +0200

    Zadanie: zmiana kodu strukt. na obektowy

---
+++ kodilla-intro/src/main/java/AppUser.java
@@ -0,0 +1,45 @@
+public class AppUser {
+    private String name;
+    private double age;
+    private double height;
+
+    public AppUser(String name, double age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getAge() {
+        return age;
+    }
+
+    public void setAge(double age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void displayUserInfo() {
+        if (name != null) {
+            if (age > 30 && height > 160) {
+                System.out.println("User is older than 30 and higher than 160cm");
+            } else {
+                System.out.println("User is younger than 30 or lower than 160cm");
+            }
+        }
+    }
+}
---
+++ kodilla-intro/src/main/java/Application.java
@@ -0,0 +1,7 @@
+public class Application {
+
+    public static void main(String[] args) {
+        AppUser app = new AppUser("Adam", 17, 169.5);
+        app.displayUserInfo();
+    }
+}
